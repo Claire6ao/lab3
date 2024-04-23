152fb0aafe8e56851da92421d31b463a3ebb8787
Claire6ao <jig029@ucsd.edu>
2024-04-23 16:20:48 -0700

    Some concise message describing the change

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,29 @@
+
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+  @Test 
+  public void testReverseInPlace() {
+    int[] input1 = {1,2,3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    assertArrayEquals(new int[]{3,2,1}, input1);
+  }
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testaverageWithoutLowest(){
+    double[] input1={2.0};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1),0.0);
+    double[] input2={1.0,2.0,3.0};
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(input2),0.0);
   }
 }
+
